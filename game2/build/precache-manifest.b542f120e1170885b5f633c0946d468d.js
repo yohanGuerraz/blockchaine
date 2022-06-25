11ba5dc6c9e44ea0ab3c94a169dbe545a7d42bed
GuerrazYohan <yohanguerraz1999@gmail.com>
2022-06-25 11:26:12 +0200

    ADD : Ajout du projet game2

---
+++ game2/build/precache-manifest.b542f120e1170885b5f633c0946d468d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f98d9d2d7dfb5a0df4c",
+    "url": "/static/css/main.68aa7a36.chunk.css"
+  },
+  {
+    "revision": "9f98d9d2d7dfb5a0df4c",
+    "url": "/static/js/main.9f98d9d2.chunk.js"
+  },
+  {
+    "revision": "fcc8be35cdd47b3116c2",
+    "url": "/static/css/1.05e0e292.chunk.css"
+  },
+  {
+    "revision": "fcc8be35cdd47b3116c2",
+    "url": "/static/js/1.fcc8be35.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45a2f0b8657a330a8838fc65fbf32ac9",
+    "url": "/static/media/brain.45a2f0b8.png"
+  },
+  {
+    "revision": "4b6a38546184c06152fd2f9db68e5bcc",
+    "url": "/index.html"
+  }
+];
